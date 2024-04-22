a9a682a1c461d12873b24a2e1674d775a5693517
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-22 19:27:55 -0300

    style footer and populated options using settings data from prismic

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,31 @@
 import WordMark from "@/app/components/WordMark";
+import { createClient } from "@/prismicio";
+import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
 
-function Footer() {
+async function Footer() {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
   return (
-    <div>
-      <WordMark />
+    <div className="flex flex-col items-center justify-between gap-6 border-t border-slate-600 px-8 py-7 md:flex-row">
+      <Link href="/">
+        <WordMark />
+        <span className="sr-only">Home Page</span>
+      </Link>
+      <nav aria-label="Footer">
+        <ul className="flex gap-6">
+          {settings.data.navigation.map((item) => (
+            <li key={item.label}>
+              <PrismicNextLink
+                field={item.link}
+                className="inline-flex min-h-11 items-center"
+              >
+                {item.label}
+              </PrismicNextLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
