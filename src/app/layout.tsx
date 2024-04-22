1799214b101cc8f8d37d7a456e646d083ce063ac
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-21 23:31:34 -0300

    added components folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 import "./globals.css";
+import { DM_Sans } from "next/font/google";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const DMSans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
+});
 
 export default function RootLayout({
   children,
@@ -8,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${DMSans.variable}`}>
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
