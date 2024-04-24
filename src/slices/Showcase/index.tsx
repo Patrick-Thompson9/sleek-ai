c6ba161f74824a5cfbacf69def95310c53a73415
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-23 21:04:15 -0300

    refined spacing on small screens in showcase slice

--- src/slices/Showcase/index.tsx
+++ src/slices/Showcase/index.tsx
@@ -37,8 +37,8 @@ const Showcase = ({ slice }: ShowcaseProps): JSX.Element => {
           ),
         }}
       />
-      <div className="mt-16 grid items-center rounded-xl border-blue-50/20 bg-gradient-to-b from-slate-50/15 p-8 backdrop-blur-sm lg:grid-cols-3 lg:py-12">
-        <div className="">
+      <div className="mt-16 grid items-center gap-8 rounded-xl border-blue-50/20 bg-gradient-to-b from-slate-50/15 p-8 backdrop-blur-sm lg:grid-cols-3 lg:gap-0 lg:py-12">
+        <div>
           <div className="w-fit rounded-lg bg-blue-500/35 p-4 text-3xl">
             <>{slice.primary.icon && icons[slice.primary.icon]}</>
           </div>
